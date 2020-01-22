20c35798c9344ab80ee4eac1ac057916f5df3508
Renan Godoy <renanfgfg@gmail.com>
2020-01-22 16:55:59 -0300

    Tela 3 prototipo

---
+++ app/src/main/java/com/alura/wiseroom/ActivityAdicionarSala.java
@@ -0,0 +1,14 @@
+package com.alura.wiseroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAdicionarSala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_sala);
+    }
+}
