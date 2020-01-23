2cdfd5448c45e7ff86ee55e2875c9ccb413ad551
Renan Godoy <renanfgfg@gmail.com>
2020-01-23 09:00:03 -0300

    Tela 4/5, funcao escolha de sala

---
+++ app/src/main/java/com/alura/wiseroom/ui/ActivityCadastroColaborador.java
@@ -0,0 +1,16 @@
+package com.alura.wiseroom.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alura.wiseroom.R;
+
+public class ActivityCadastroColaborador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_colaborador);
+    }
+}
--- app/src/main/java/com/alura/wiseroom/ui/MainActivity.java
+++ app/src/main/java/com/alura/wiseroom/ui/MainActivity.java
@@ -2,15 +2,37 @@ package com.alura.wiseroom.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.alura.wiseroom.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btLogin;
+    private Button btCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btLogin = findViewById(R.id.btLogin);
+        btCadastro = findViewById(R.id.btCadastro);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ActivityEscolha.class));
+            }
+        });
+
+        btCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ActivityCadastroColaborador.class));
+            }
+        });
+
+
     }
 }
