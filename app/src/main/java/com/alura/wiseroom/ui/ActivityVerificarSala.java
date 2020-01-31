6c96e35b8e3bfb7f819138997448e4642b0f3b32
Renan Godoy <renanfgfg@gmail.com>
2020-01-31 15:30:43 -0300

    verificacao salas reservadas com json

--- app/src/main/java/com/alura/wiseroom/ui/ActivityVerificarSala.java
+++ app/src/main/java/com/alura/wiseroom/ui/ActivityVerificarSala.java
@@ -2,9 +2,6 @@ package com.alura.wiseroom.ui;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,31 +9,37 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alura.wiseroom.R;
-import com.alura.wiseroom.database.WiseRoomDB;
 import com.alura.wiseroom.model.ColaboradorModel;
 import com.alura.wiseroom.model.SalaModel;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ActivityVerificarSala extends AppCompatActivity {
     final Activity activity = this;
     ColaboradorModel colaboradorLogado;
-    SQLiteDatabase db;
-    Cursor cursor;
-    SQLiteOpenHelper dbHelper;
+    RequestQueue mQueue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escolher_sala);
+        mQueue = Volley.newRequestQueue(this);
         IntentIntegrator intentIntegrator = new IntentIntegrator(activity);
         intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
         intentIntegrator.setPrompt("Escolha a sala");
         intentIntegrator.setCameraId(0);
         intentIntegrator.initiateScan();
-        dbHelper = new WiseRoomDB(this);
-        db = dbHelper.getWritableDatabase();
         recebeDados();
     }
 
@@ -45,36 +48,15 @@ public class ActivityVerificarSala extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
 
+
+
         if(intentResult != null){
             if (intentResult.getContents() !=  null){
 
-                Log.i("teste qr ",  intentResult.getContents());
+                Log.i("teste qr ", intentResult.getContents());
+                String idSalas = intentResult.getContents();
 
-                cursor = db.rawQuery("SELECT * FROM " + WiseRoomDB.TABELA_NOME_SALA + " WHERE " +
-                        WiseRoomDB.COLUNA_ID_SALA + "=?", new String[]{intentResult.getContents()});
-                if (cursor != null) {
-                    if (cursor.getCount() > 0) {
-
-                        cursor.moveToFirst();
-
-                        String intentId = cursor.getString(cursor.getColumnIndex(WiseRoomDB.COLUNA_ID_SALA));
-                        String intentNome = cursor.getString(cursor.getColumnIndex(WiseRoomDB.COLUNA_NOME_SALA));
-                        int intentCapacidade = cursor.getInt(cursor.getColumnIndex(WiseRoomDB.COLUNA_CAPACIDADE_SALA));
-                        String intentDescricao = cursor.getString(cursor.getColumnIndex(WiseRoomDB.COLUNA_DESCRICAO_SALA));
-
-                        SalaModel salaSelecionada = new SalaModel();
-                        salaSelecionada.setId(intentId);
-                        salaSelecionada.setNome(intentNome);
-                        salaSelecionada.setCapacidade(intentCapacidade);
-                        salaSelecionada.setDescricaoSala(intentDescricao);
-
-
-                        Intent intent = new Intent(ActivityVerificarSala.this, ActivityDatasReservadas.class);
-                        intent.putExtra("salaSelecionada", salaSelecionada);
-                        intent.putExtra("colaboradorLogado", colaboradorLogado);
-                        startActivity(intent);
-                    }
-                }
+                verificaSala(idSalas);
 
             }else{
 
@@ -96,6 +78,47 @@ public class ActivityVerificarSala extends AppCompatActivity {
             colaboradorLogado = col;
 
         }
+    }
+
+    public void verificaSala(String idSala){
+        String url = "http://172.30.248.130:3000/sala?id="+idSala;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray resposta) {
+                        if (resposta.length() > 0) {
+                            try {
+                                for (int i = 0; i < resposta.length(); i++) {
+
+                                    JSONObject salaJson = resposta.getJSONObject(i);
+                                    SalaModel salaRecebidaJson = new SalaModel();
+
+                                    salaRecebidaJson.setId(salaJson.getString("id"));
+                                    salaRecebidaJson.setNome(salaJson.getString("nome"));
+                                    salaRecebidaJson.setCapacidade(salaJson.getInt("capacidade"));
+                                    salaRecebidaJson.setDescricaoSala(salaJson.getString("descricao"));
+                                    salaRecebidaJson.setAreaDaSala(salaJson.getDouble("area"));
+
+                                    Log.i("TESTE RODANDO ?? ", "model objeto " + salaRecebidaJson.toString());
+
+                                    Intent intent = new Intent(ActivityVerificarSala.this, ActivityDatasReservadas.class);
+                                    intent.putExtra("colaboradorLogado", colaboradorLogado);
+                                    intent.putExtra("salaSelecionada", salaRecebidaJson);
+                                    startActivity(intent);
+
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }}
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 
 }
