652199db527939153982616720db8a8af8f5dc40
Renan Godoy <renanfgfg@gmail.com>
2020-02-06 10:58:10 -0300

    asdasdasdasdad

--- app/src/main/java/com/alura/wiseroom/model/FilialModel.java
+++ app/src/main/java/com/alura/wiseroom/model/FilialModel.java
@@ -2,15 +2,7 @@ package com.alura.wiseroom.model;
 
 public class FilialModel extends OrganizacaoModel {
    private String idMatriz;
-   private String nomeFilial;
 
-    public String getNomeFilial() {
-        return nomeFilial;
-    }
-
-    public void setNomeFilial(String nomeFilial) {
-        this.nomeFilial = nomeFilial;
-    }
 
     public String getIdMatriz() {
         return idMatriz;
