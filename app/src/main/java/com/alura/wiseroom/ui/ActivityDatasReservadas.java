b576752b99a9ee8a93fdfa1f9929b2b8dd2deaea
Renan Godoy <renanfgfg@gmail.com>
2020-02-20 15:11:27 -0300

    lista datas + event bus

--- app/src/main/java/com/alura/wiseroom/ui/ActivityDatasReservadas.java
+++ app/src/main/java/com/alura/wiseroom/ui/ActivityDatasReservadas.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -12,10 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.alura.wiseroom.R;
 import com.alura.wiseroom.adapter.ReservaAdapter;
+import com.alura.wiseroom.constants.Constants;
 import com.alura.wiseroom.model.ColaboradorModel;
+import com.alura.wiseroom.model.Event;
 import com.alura.wiseroom.model.OrganizacaoModel;
 import com.alura.wiseroom.model.ReservaModel;
 import com.alura.wiseroom.model.SalaModel;
+import com.alura.wiseroom.network.HttpRequest;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,9 +28,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -43,14 +50,12 @@ public class ActivityDatasReservadas extends AppCompatActivity {
     ReservaAdapter reservaAdapter;
     ArrayList<ReservaModel> listaReservas = new ArrayList<>();
     ArrayList<String> listIds = new ArrayList<>();
-   // ArrayList<ColaboradorModel> listaColaboradores = new ArrayList<>();
+    // ArrayList<ColaboradorModel> listaColaboradores = new ArrayList<>();
     boolean flagEditAlarm = false;
     SalaModel salaSelecioanda;
     ColaboradorModel colaboradorLogado;
     RequestQueue mQueue;
     ColaboradorModel colaboradorBodega;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,36 +78,50 @@ public class ActivityDatasReservadas extends AppCompatActivity {
 
     }
 
-    private void mostraDescricaoReserva(int posicao){
+
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+        if (flagEditAlarm == true) {
+            fetchDatabaseToArrayList();
+            flagEditAlarm = false;
+        }
+    }
+
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    private void mostraDescricaoReserva(int posicao) {
 
     }
 
-    private void init () {
+    private void init() {
         listView = (ListView) findViewById(R.id.listViewY);
     }
 
     private void fetchDatabaseToArrayList() {
 
-            listaReservas.clear();
-            listIds.clear();
-            verificaReserva(salaSelecioanda.getIdSala());
-            reservaAdapter = new ReservaAdapter(ActivityDatasReservadas.this, R.layout.item_lista_reserva, listaReservas);
-            listView.setAdapter(reservaAdapter);
-
+        listaReservas.clear();
+        listIds.clear();
+        verificaReserva(salaSelecioanda.getIdSala());
+        reservaAdapter = new ReservaAdapter(ActivityDatasReservadas.this, R.layout.item_lista_reserva, listaReservas);
+        listView.setAdapter(reservaAdapter);
 
     }
 
     private void recebeDados() {
         Intent intentMain = getIntent();
 
-        if(intentMain.hasExtra("colaboradorLogado")){
+        if (intentMain.hasExtra("colaboradorLogado")) {
 
             ColaboradorModel col = (ColaboradorModel) intentMain.getSerializableExtra("colaboradorLogado");
             colaboradorLogado = col;
 
         }
 
-        if(intentMain.hasExtra("salaSelecionada")){
+        if (intentMain.hasExtra("salaSelecionada")) {
 
             SalaModel sal = (SalaModel) intentMain.getSerializableExtra("salaSelecionada");
             salaSelecioanda = sal;
@@ -111,135 +130,68 @@ public class ActivityDatasReservadas extends AppCompatActivity {
     }
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(flagEditAlarm==true) {
-            fetchDatabaseToArrayList();
-            flagEditAlarm=false;
+    public void verificaReserva(final String idSala) {
+
+        try {
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("authorization", "secret");
+            params.put("idSala", idSala);
+            String url = "http://172.30.248.130:8080/ReservaDeSala/rest/reserva/byIdSala";
+
+            new HttpRequest(
+                    getApplicationContext(),
+                    params,
+                    url,
+                    "GET", "DataReservada").doRequest();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public void verificaReserva(final String idSala){
-        String url = "http://172.30.248.130:8080/ReservaDeSala/rest/reserva/byIdSala";
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String resposta) {
-                        try {
-                            Log.i("teste responsee", resposta);
+    @Subscribe
+    public void customEventReceived(Event event) {
+        if (event.getEventName().equals("Login" + Constants.eventSuccessLabel)) {
+            Log.i("teste responsee", event.getEventMsg());
 
-                            Gson gson = new Gson();
+            Gson gson = new Gson();
 
+            Type listType = new TypeToken<List<ReservaModel>>() {
+            }.getType();
+            List<ReservaModel> reservas = gson.fromJson(event.getEventMsg(), listType);
 
-                            Type listType = new TypeToken<List<ReservaModel>>() {
-                            }.getType();
-                            List<ReservaModel> reservas = gson.fromJson(resposta, listType);
+            for (int i = 0; i < reservas.size(); i++) {
 
+                ReservaModel reservaRecebidaJson = new ReservaModel();
 
-                            for (int i = 0; i < reservas.size(); i++) {
+                reservaRecebidaJson = reservas.get(i);
+                Log.i("teste reserva ", reservas.get(i).toString());
 
-                                ReservaModel reservaRecebidaJson = new ReservaModel();
+                reservaRecebidaJson.setIdReserva(reservas.get(i).getIdReserva());
+                reservaRecebidaJson.setDescricaoReserva(reservas.get(i).getDescricaoReserva());
+                reservaRecebidaJson.setDataReserva(reservas.get(i).getDataReserva());
+                reservaRecebidaJson.setHoraInicioReserva(reservas.get(i).getHoraInicioReserva());
+                reservaRecebidaJson.setHoraFimReserva(reservas.get(i).getHoraFimReserva());
+                reservaRecebidaJson.setIdColaborador(reservas.get(i).getIdColaboradorReserva());
 
-                                reservaRecebidaJson = reservas.get(i);
-                                Log.i("teste reserva ", reservas.get(i).toString());
-
-                                reservaRecebidaJson.setIdReserva(reservas.get(i).getIdReserva());
-                                reservaRecebidaJson.setDescricaoReserva(reservas.get(i).getDescricaoReserva());
-                                reservaRecebidaJson.setDataReserva(reservas.get(i).getDataReserva());
-                                reservaRecebidaJson.setHoraInicioReserva(reservas.get(i).getHoraInicioReserva());
-                                reservaRecebidaJson.setHoraFimReserva(reservas.get(i).getHoraFimReserva());
-                                reservaRecebidaJson.setIdColaborador(reservas.get(i).getIdColaboradorReserva());
-
-                           //     recebeColaborador(reservaRecebidaJson.getIdColaboradorReserva());
+                //     recebeColaborador(reservaRecebidaJson.getIdColaboradorReserva());
 
 
-                             //   reservaRecebidaJson.setColaboradorReserva(colaboradorBodega);
-                                reservaRecebidaJson.setSalaReserva(salaSelecioanda);
+                //   reservaRecebidaJson.setColaboradorReserva(colaboradorBodega);
+                reservaRecebidaJson.setSalaReserva(salaSelecioanda);
 
-                                Log.i("teste bodega", reservaRecebidaJson.getSalaReserva().toString());
-                                listaReservas.add(reservaRecebidaJson);
-                                listIds.add(reservaRecebidaJson.getIdReserva());
+                Log.i("teste bodega", reservaRecebidaJson.getSalaReserva().toString());
+                listaReservas.add(reservaRecebidaJson);
+                listIds.add(reservaRecebidaJson.getIdReserva());
 
-                            }
-                        } catch (Exception e){e.printStackTrace();}
-                    }
-
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
             }
-        })
-        {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError
-            {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("authorization", "secret");
-                params.put("idSala", idSala);
-
-                return params;
+        } else if (event.getEventName().equals("Login" + Constants.eventErrorLabel)) {
+                Snackbar snackbar = Snackbar.make(null, "Erro ao realizar login", Snackbar.LENGTH_LONG);
+                snackbar.getView().getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+                snackbar.show();
             }
-        };
-
-
-        mQueue.add(request);
+        }
     }
 
-    /* public void recebeColaborador(final String idColaborador){
-        String url = "http://172.30.248.130:8080/ReservaDeSala/rest/colaborador/byIdColaborador";
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String resposta) {
-                        try {
-                            Log.i("response col ", resposta);
-                            Gson gson = new Gson();
-
-                            ColaboradorModel colaboradorJson = gson.fromJson(resposta, ColaboradorModel.class);
-                            ColaboradorModel colaboradorRecebidoJson = new ColaboradorModel();
-
-                            colaboradorRecebidoJson.setIdColaborador(colaboradorJson.getIdColaborador());
-                            colaboradorRecebidoJson.setNomeColaborador(colaboradorJson.getNomeColaborador());
-                            colaboradorRecebidoJson.setEmailColaborador(colaboradorJson.getEmailColaborador());
-                            colaboradorRecebidoJson.setAdministrador(colaboradorJson.isAdministrador());
-
-                            //  OrganizacaoModel organizacaoJson = colaboradorJson.getOrganizacaoColaborador();
-                            //      OrganizacaoModel organizacaoModel = new OrganizacaoModel();
-
-                            //      organizacaoModel.setIdOrganizacao(organizacaoJson.getIdOrganizacao());
-                            //      organizacaoModel.setNomeOrganizacao(organizacaoJson.getNomeOrganizacao());
-                            //      organizacaoModel.setDominioOrganizacao(organizacaoJson.getDominioOrganizacao());
-                            //      colaboradorRecebidoJson.setOrganizacaoColaborador(organizacaoModel);
-
-                            colaboradorBodega = new ColaboradorModel();
-                            colaboradorBodega = colaboradorRecebidoJson;
-                        } catch(Exception e){e.printStackTrace();}
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        })
-
-        {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError
-            {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("authorization", "secret");
-                params.put("idColaborador", idColaborador);
-
-                return params;
-            }
-        };
-        mQueue.add(request);
-
-    } */
-
-}
 
 
 
